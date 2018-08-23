000cbcef824284826ab78d372fab7341104049f5
ParthPate1 <35845631+ParthPate1@users.noreply.github.com>
2018-08-23 11:41:05 -0500

    hack01

---
+++ hello.c
@@ -0,0 +1,13 @@
+/*
+Name:Parth Patel
+Date:08-23-2018
+Description: hack01 hello world
+*/
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char **argv) {
+  printf("Parth Patel, major: computer engineering\n");
+
+  return 0;
+}
